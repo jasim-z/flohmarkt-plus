de42a178c25925f16360e319656a64da380a9195
code-odyssey-trunk <jablog2020@gmail.com>
2025-07-18 09:18:22 +0200

    Feature: Implement Sign up and Login

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
